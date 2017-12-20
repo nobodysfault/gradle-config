44ef3d1d2ea0bcc2c4a021936802753d2d420a7e
Vladimir <nonobodysfault@gmail.com>
2017-12-20 12:05:08 +0300

    Make plugin work with Android libraries too

--- lib/src/main/groovy/com/tmiyamon/config/ConfigPlugin.groovy
+++ lib/src/main/groovy/com/tmiyamon/config/ConfigPlugin.groovy
@@ -5,25 +5,35 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import com.android.build.gradle.api.BaseVariant
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.LibraryExtension
 
 class ConfigPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.plugins.withId('com.android.application') {
-            def android = project.extensions.getByType(AppExtension)
-            android.applicationVariants.all { BaseVariant variant ->
-
-                def task = project.tasks.create(
+        def processVariant = { extension, BaseVariant variant ->
+            def task = project.tasks.create(
                     name: "generate${variant.name.capitalize()}Settings",
                     type: GenerateSettingsTask) {
-                        packageName    variant.generateBuildConfig.buildConfigPackageName
-                        flavorName     variant.flavorName
-                        buildTypeName  variant.buildType.name
-                        variantDirName variant.dirName
-                    }
+                packageName    variant.generateBuildConfig.buildConfigPackageName
+                flavorName     variant.flavorName
+                buildTypeName  variant.buildType.name
+                variantDirName variant.dirName
+            }
 
-                variant.registerJavaGeneratingTask(task, task.outputDir())
-                android.sourceSets[variant.name].java.srcDirs += [task.outputDir()]
+            variant.registerJavaGeneratingTask(task, task.outputDir())
+            extension.sourceSets[variant.name].java.srcDirs += [task.outputDir()]
+        }
+
+        project.plugins.withId('com.android.application') {
+            def app = project.extensions.findByType(AppExtension)
+            if (app != null) {
+                app.applicationVariants.all(processVariant.curry(app))
+            }
+        }
+        project.plugins.withId('com.android.library') {
+            def library = project.extensions.findByType(LibraryExtension)
+            if (library != null) {
+                library.libraryVariants.all(processVariant.curry(library))
             }
         }
     }
